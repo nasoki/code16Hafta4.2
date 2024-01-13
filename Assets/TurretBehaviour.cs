ceaafe6b31699734eeb0f209439eb898103c806f
Mehmet Berat Bektas <will.98.black@hotmail.com>
2024-01-13 18:17:56 +0300

    forward backwards

--- Assets/TurretBehaviour.cs
+++ Assets/TurretBehaviour.cs
@@ -9,10 +9,12 @@ public class TurretBehaviour : MonoBehaviour
     public GameObject weaponHead;
     public GameObject tank;
     Vector2 weaponRotation;
+    float speedMultiplier = 5f;
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        Application.targetFrameRate = 60;
     }
 
     // Update is called once per frame
@@ -24,6 +26,9 @@ public class TurretBehaviour : MonoBehaviour
         weaponBase.transform.rotation = Quaternion.Euler(0, weaponRotation.x, 0);
         weaponHead.transform.rotation = Quaternion.Euler(weaponRotation.y, weaponRotation.x, 0);
         tank.transform.Rotate(0, Input.GetAxis("Horizontal"), 0);
+        Vector3 playerForward = new Vector3(0f, 0f, Input.GetAxis("Vertical"));
+        Vector3 hiz = playerForward * Time.deltaTime * speedMultiplier;
+        transform.Translate(hiz);
         //todo - ileri hareket, hedef alma, tagli objeleri yok etme
         //tank.GetComponent<Rigidbody>().rotation = Quaternion.Euler(0, Input.GetAxis("Horizontal") + 100, 0);
     }
